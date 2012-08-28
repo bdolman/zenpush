88ee3deaf65543a2d2468a04f1922b275d464b3d
Ben Dolman <ben.dolman@gmail.com>
2012-08-28 17:59:41 -0600

    Enable all Redcarpet markdown extensions.
    This allows for table syntax among other features. The end result is basically GFM (Github Flavored Markdown).

--- lib/zenpush/markdown.rb
+++ lib/zenpush/markdown.rb
@@ -7,7 +7,18 @@ module ZenPush
     # Convert a markdown file to HTML, removing all <code> tags,
     # which make Zendesk remove carriage returns.
     def self.to_zendesk_html(file)
-      ::Markdown.new(File.read(file)).to_html.gsub(/<\/?code>/, '')
+      markdown_extensions = {
+        :no_intra_emphasis => true, 
+        :tables => true, 
+        :fenced_code_blocks => true, 
+        :autolink => true, 
+        :strikethrough => true, 
+        :lax_html_blocks => true, 
+        :space_after_headers => true,
+        :superscript => true
+      }
+      markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, markdown_extensions)
+      markdown.render(File.read(file))
     end
   end
 end
